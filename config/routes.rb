c7371b08da320084782d94e76fef03cf70da8e09
鈴木義晴 <>
2018-06-16 11:44:05 +0900

    test

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get 'comments/destroy'
 
   devise_for :users
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   resources :news
   resources :energies
   resources :houses do
